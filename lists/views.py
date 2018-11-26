783a845eb1fc87dd3fe1b63bd9e320f8d688b6d2
skywalker <maverick.fengys@gmail.com>
2018-11-26 12:43:54 +0800

    Enforce model validation in list view.

--- lists/views.py
+++ lists/views.py
@@ -10,10 +10,15 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
     if request.method == 'POST':
         item = Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/{0}/'.format(list_id))
-    return render(request, 'list.html', {'list': list_})
+        try:
+            item.full_clean()
+            return redirect('/lists/{0}/'.format(list_id))
+        except ValidationError as e:
+            error = "You can't have an empty list item"
+    return render(request, 'list.html', {'list': list_, 'error':error})
 
 
 def new_list(request):
