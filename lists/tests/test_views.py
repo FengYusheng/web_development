1eb1a4efdef6e1cc565d5b6a17a5197dc3adb95b
skywalker <maverick.fengys@gmail.com>
2018-11-26 21:38:16 +0800

    Use new form in home_page, simpilfy tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,10 +4,12 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
+    maxDiff = None
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -16,10 +18,20 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', {'form':ItemForm()})
         # print(expected_html)
         # TODO: remove csrf, https://stackoverflow.com/questions/34629261/django-render-to-string-ignores-csrf-token#34629713
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertMultiLineEqual(response.content.decode(), expected_html)
+
+
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 
